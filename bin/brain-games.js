4c24ef94444fa2ef949a3a657af732c458b0e0ba
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-01-25 18:14:46 +0300

    changed brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
 import { greetandask } from '../src/cli.js';
 
-const greetFunc = () => {
-  console.log('Welcome to the Brain Games!');
-};
+// const greetFunc = () => {
+//  console.log('Welcome to the Brain Games!');
+// };
 greetandask();
