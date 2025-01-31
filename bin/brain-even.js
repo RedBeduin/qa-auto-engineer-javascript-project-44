35cbf1fccc1e214c9f639d6e008dc2b4976e8e30
Vyacheslav Strizhius <v.v.strizhius@gmail.com>
2025-01-31 16:04:55 +0300

    changed executable file and file with game logic for brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
 import engine from '../src/engine.js';
-// import { sayEvenOrNot, brainEvenQuestion } from '../src/brain-even.js';
-
-const brainEvenQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-
-  return false;
-};
-
-const sayEvenOrNot = (num) => ((isEven(num)) ? ['yes', num] : ['no', num]);
+import { sayEvenOrNot, brainEvenQuestion } from '../src/brain-even.js';
 
 engine(sayEvenOrNot, brainEvenQuestion);
